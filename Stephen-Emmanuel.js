394ac6d0047c2d530bf337443300e685838b410b
Steph-crown <49078472+Steph-crown@users.noreply.github.com>
2020-06-03 15:29:56 +0100

    Add files via upload

---
+++ Stephen-Emmanuel.js
@@ -0,0 +1,2 @@
+let name = "Stephen Emmanuel", id = "HNG-06244", email = "emmanuelstephen024@gmail.com", lang = "Javascript"
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${lang} for stage 2 task`)
